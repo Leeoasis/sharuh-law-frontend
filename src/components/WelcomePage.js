dfa6a0cc78ffde97a5e23e975117a4438c8d67b6
Leslie <Kooliesk@gmail.com>
2024-11-20 15:46:45 +0200

    Setup tailwind and basic structure

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,13 @@
+const WelcomePage = () => {
+    return (
+        <div className="bg-white">
+            <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
+                <div className="lg:text-center">
+                    <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Welcome to Sharuh Law</h2>
+                    </div>
+            </div>
+        </div>
+    )
+};
+
+export default WelcomePage;
