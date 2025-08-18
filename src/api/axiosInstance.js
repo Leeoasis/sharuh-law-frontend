9919ad35a58619daadb6915438ed545e551451b2
Leslie <Kooliesk@gmail>
2025-08-18 11:32:01 +0200

    update the url for fetching requests

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:3001', // Ensure this URL is correct
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'https://sharuh-law-backend.onrender.com', 
+  // Fallback to Render URL if env variable is not set
 });
 
 axiosInstance.interceptors.request.use(
